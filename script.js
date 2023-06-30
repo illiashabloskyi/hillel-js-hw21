5619fe47ba974b170bb5fb0300427a3d7c7a2540
shablovskyi_illia <shablovskyiillia@gmail.com>
2023-06-30 16:22:18 +0300

    create structure and update script.js

---
+++ script.js
@@ -0,0 +1,54 @@
+class SuperMath {
+  check(obj) {
+    const { X, Y, znak } = obj;
+
+    const confirmation = prompt(`Do you want to perform the operation ${X} ${znak} ${Y}?`);
+    if(confirmation === 'yes' || confirmation === "y"){
+      if (this.isValidOperator(znak)) {
+        const result = this.calculate(X, Y, znak);
+        console.log("Result: " + result);
+      } else {
+        console.log("Invalid operator!");
+      }
+    }else{
+      this.input(obj);
+    }
+
+  }
+
+  input(obj){
+    const X = parseFloat(prompt("Enter X:"))
+    const Y = parseFloat(prompt("Enter Y:"))
+    const znak = prompt("Enter operator:")
+
+    const newObj = {X: X, Y: Y, znak};
+    this.check(newObj)
+  }
+
+  isValidOperator(operator){
+    const isValidOperator = ['+', '-', '/', '*', '%']
+    return isValidOperator.includes(operator)
+  }
+
+  calculate(X, Y, znak) {
+    switch (znak) {
+      case "+":
+        return X + Y;
+      case "-":
+        return X - Y;
+      case "/":
+        return X / Y;
+      case "*":
+        return X * Y;
+      case "%":
+        return X % Y;
+      default:
+        return NaN;
+    }
+  }
+
+}
+obj = {X:12, Y:3, znak: '-'}
+p = new SuperMath();
+p.check(obj);
+
