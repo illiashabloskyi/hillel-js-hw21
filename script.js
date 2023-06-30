b31dbc6e2c5dc6d66bdc864424cb8c138f15d3eb
shablovskyi_illia <shablovskyiillia@gmail.com>
2023-06-30 16:27:06 +0300

    update script.js

--- script.js
+++ script.js
@@ -2,32 +2,33 @@ class SuperMath {
   check(obj) {
     const { X, Y, znak } = obj;
 
-    const confirmation = prompt(`Do you want to perform the operation ${X} ${znak} ${Y}?`);
-    if(confirmation === 'yes' || confirmation === "y"){
+    const confirmation = prompt(
+      `Do you want to perform the operation ${X} ${znak} ${Y}?`
+    );
+    if (confirmation === "yes" || confirmation === "y") {
       if (this.isValidOperator(znak)) {
         const result = this.calculate(X, Y, znak);
         console.log("Result: " + result);
       } else {
         console.log("Invalid operator!");
       }
-    }else{
+    } else {
       this.input(obj);
     }
-
   }
 
-  input(obj){
-    const X = parseFloat(prompt("Enter X:"))
-    const Y = parseFloat(prompt("Enter Y:"))
-    const znak = prompt("Enter operator:")
+  input(obj) {
+    const X = parseFloat(prompt("Enter X:"));
+    const Y = parseFloat(prompt("Enter Y:"));
+    const znak = prompt("Enter operator:");
 
-    const newObj = {X: X, Y: Y, znak};
-    this.check(newObj)
+    const newObj = { X: X, Y: Y, znak };
+    this.check(newObj);
   }
 
-  isValidOperator(operator){
-    const isValidOperator = ['+', '-', '/', '*', '%']
-    return isValidOperator.includes(operator)
+  isValidOperator(operator) {
+    const isValidOperator = ["+", "-", "/", "*", "%"];
+    return isValidOperator.includes(operator);
   }
 
   calculate(X, Y, znak) {
@@ -46,9 +47,7 @@ class SuperMath {
         return NaN;
     }
   }
-
 }
-obj = {X:12, Y:3, znak: '-'}
+obj = { X: 12, Y: 3, znak: "-" };
 p = new SuperMath();
 p.check(obj);
-
